f5aa819b8cd00a77c8d5f731eb7b57abfb9556b2
Muyuan Cao <cmyhamster@gmail.com>
2021-02-10 01:53:52 -0500

    s

--- a1_extractFeatures.py
+++ a1_extractFeatures.py
@@ -279,7 +279,27 @@ def main(args):
         comment_file = data[i]['cat']
         commentID = data[i]['id']
         feats[i][:-1] = extract1(comment)
-        feats[i][:-1] = extract2(feats[i][:-1], comment_file, commentID)
+        # feats[i][:-1] = extract2(feats[i][:-1], comment_file, commentID)
+        if data[i]['cat'] == 'Left':
+            feats[i][-1] = 0
+            index = Left_ID.index(data[i]['id'])
+            feats[i][29:-1] = Left_data[index][:]
+
+        if data[i]['cat'] == 'Center':
+            feats[i][-1] = 1
+            index = Center_ID.index(data[i]['id'])
+            feats[i][29:-1] = Center_data[index][:]
+
+        if data[i]['cat'] == 'Right':
+            feats[i][-1] = 2
+            index = Right_ID.index(data[i]['id'])
+            feats[i][29:-1] = Right_data[index][:]
+
+        if data[i]['cat'] == 'Alt':
+            feats[i][-1] = 3
+            index = Alt_ID.index(data[i]['id'])
+            feats[i][29:-1] = Alt_data[index][:]
+
         if i % 100 == 0:
             print(i)
 
