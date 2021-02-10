97786167c0f806174d1560abe946036a2b6f3235
Muyuan Cao <cmyhamster@gmail.com>
2021-02-10 00:50:28 -0500

    part2

--- a1_extractFeatures.py
+++ a1_extractFeatures.py
@@ -9,15 +9,14 @@
 #  * Copyright (c) 2020 Frank Rudzicz
 
 import numpy as np
+import sys
 import argparse
-import json
-import pandas as pd
-import re
-import functools
 import os
-import time
-#from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD
-#from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import json
+import string
+import csv
+import re
+
 
 # Provided wordlists.
 FIRST_PERSON_PRONOUNS = {
@@ -34,51 +33,45 @@ SLANG = {
     'afn', 'bbs', 'cya', 'ez', 'f2f', 'gtr', 'ic', 'jk', 'k', 'ly', 'ya',
     'nm', 'np', 'plz', 'ru', 'so', 'tc', 'tmi', 'ym', 'ur', 'u', 'sol', 'fml'}
 
+path = '/u/cs401/A1/feats/'
+Alt_data = np.load(path + 'Alt_feats.dat.npy')  # <class 'numpy.ndarray'>   (200272, 144)
+Alt_ID = open(path + 'Alt_IDs.txt', 'r').read().split('\n')  # List of IDs
 
-def len_dec(fn):  # decorator to automatically take the length of a list returned by 'fn'
-  def len_fn(*args, **kwargs):
-    return len(fn(*args, **kwargs))
-  return len_fn
+Right_data = np.load(path + 'Right_feats.dat.npy')  # <class 'numpy.ndarray'>   (200272, 144)
+Right_ID = open(path + 'Right_IDs.txt', 'r').read().split('\n')  # List of IDs
 
+Left_data = np.load(path + 'Left_feats.dat.npy')  # <class 'numpy.ndarray'>   (200272, 144)
+Left_ID = open(path + 'Left_IDs.txt', 'r').read().split('\n')  # List of IDs
 
-# Global vars
-findall = functools.partial(re.findall, flags=re.IGNORECASE)  # assume we ignore case unless told otherwise.
-nfindall = len_dec(findall)  # nfindall return number of matches from findall
+Center_data = np.load(path + 'Center_feats.dat.npy')  # <class 'numpy.ndarray'>   (200272, 144)
+Center_ID = open(path + 'Center_IDs.txt', 'r').read().split('\n')  # List of IDs
 
-a1_dir = "/u/cs401/A1/"  # can't acess args...
-# Files opened once to save time.
-wordlists_dir = "/u/cs401/Wordlists"
-bgl = pd.read_csv(os.path.join(wordlists_dir, "BristolNorms+GilhoolyLogie.csv"),
-                  usecols=["WORD", "AoA (100-700)", "IMG", "FAM"])  # select just needed cols
-warr = pd.read_csv(os.path.join(wordlists_dir, "Ratings_Warriner_et_al.csv"),
-                   usecols=["Word", "V.Mean.Sum", "D.Mean.Sum", "A.Mean.Sum"])  # select just needed cols
+BGL_csv_filename = '/u/cs401/Wordlists/BristolNorms+GilhoolyLogie.csv'
+Warringer_csv_filename = '/u/cs401/Wordlists/Ratings_Warriner_et_al.csv'
 
-bgl2 = bgl.set_index(['WORD'])  # set the index to the word columns for faster retrieval later.
-warr2 = warr.set_index(['Word'])
+file = open(BGL_csv_filename, 'r')
+reader = csv.reader(file)
+word1 = []
+AoA = []
+IMG = []
+FAM = []
+for line in reader:
+    word1.append(line[1])
+    AoA.append(line[3])
+    IMG.append(line[4])
+    FAM.append(line[5])
 
-feats_base = os.path.join(a1_dir, 'feats')
-files = ['Left', 'Center', 'Right', 'Alt']  # files for different labels, in order.
-files = {f: [open(os.path.join(feats_base, f + '_IDs.txt')).readlines(),  # [0] is the IDs
-             np.load(os.path.join(feats_base, f + '_feats.dat.npy')),  # [1] is the features
-             i] for i, f in enumerate(files)  # [2] is the integer label
-         }
-
-for v in files.values():
-    # remove the [2] entry and concatenate to features so that the -1 index is the label as desired.
-    v[1] = np.concatenate([v[1], np.ones((len(v[1]), 1)) * v.pop()], axis=1)
-
-# files[$file][0] is a list of strings where each line is an ID (result of readlines).
-# we turn this into a dict where each ID is a key, and value is the line number,
-# so that we can access it easier later (in O(1) time).
-for k in files:
-    id_line = {}
-    for i, line in enumerate(files[k][0]):
-        id_line[str(line).strip()] = i
-    files[k][0] = id_line
-
-# Now files is a dict, where the key is the file name, and the value is a list where,
-# list[0] is a dict holding the id: line # relationship
-# list[1] are the feature values, with the label appended to the end already.
+file = open(Warringer_csv_filename, "r")
+reader = csv.reader(file)
+word2 = []
+valence = []
+arousal = []
+dominance = []
+for line in reader:
+    word2.append(line[1])
+    valence.append(line[2])
+    arousal.append(line[5])
+    dominance.append(line[8])
 
 
 def extract1(comment):
@@ -92,153 +85,144 @@ def extract1(comment):
   Returns:
       feats : numpy Array, a 173-length vector of floating point features (only the first 29 are expected to be filled, here)
   '''
-  def flatten(arr):  # takes an pd.Series objects due to duplicate word entries in the provided
-    # files (they exist) and flattens it to separate entries so that our final array
-    # only has float values.
-    new_arr = []
-    for a in arr:
-      if isinstance(a, pd.Series):
-        new_arr.extend(a.values)
-      else:
-        new_arr.append(a)
-    return new_arr
-  features = np.zeros(173)  #
-  # The patters are a list of regex patterns we will process in a for loop
-  patterns = [(r"([A-Z]\w{2,})/[A-Z]{2,4}", {'flags': 0}),  # 1. n uppercase > 3, case-sensitive
-              (r"(?<=\b)(?:" + "|".join(FIRST_PERSON_PRONOUNS) + ")(?=\/|\b)",
-               {}),  # 2. n first-person pronouns
-              (r"(?<=\b)(?:" + "|".join(SECOND_PERSON_PRONOUNS) + ")(?=\b|\/)",
-               {}),  # 3. n second-person pronouns
-              (r"(?<=\b)(?:" + "|".join(THIRD_PERSON_PRONOUNS) + ")(?=\b|\/)",
-               {}),  # 4. n third-person pronouns
-              (r"(/CC)(?=\b)", {}),  # 5. n coordinating conjunctions
-              (r"(/VBD)(?=\b)", {}),  # 6. n past-tense verbs
-              (
-              r"(?:(?:going|gonna|will|'ll)(?:/?.{0,4})|go/VBG)\s+to(?:/.{0,2})\s+(\w*/VB)(?=\b|/)",
-              {}),  # 7. n future tense, .{} are for tags.
-              (r",/,|(?<!/),", {}),  # 8. n commas
-              #(r"[`~!@#$%\^&\*\(\):;\"\'\\\+,\-\.<>=?\[\]_\{\|\}']{2,}", {}),
-              (r"([\#\$\!\?\.\:\;\(\)\"\',]{2,}|\.\.\.)", {}),
-              # 9. n multchar punctuations
-              (r"(?:/NN|/NNS)(?=\b)", {}),  # 10. n common nouns
-              (r"(?:/NNP|/NNPS)(?=\b)", {}),  # 11.n proper nouns
-              (r"(?:/RB|/RBR|/RBS)(?=\b)", {}),  # 12. n adverbs
-              (r"(?:/WDT\b|/WP\$\W|/WRB\b|/WP\b)", {}),  # 13. n wh-words
-              # (r"(\b"+r"|".join(SLANG)+")(?:/[.]{0,4})", {}),
-              (r"(?:\s|^)("+r"|".join(SLANG)+")(?:/[.]{0,4})", {})  # 14. n slang words
-              ]
-  word_tags = comment.split()  # as per preprocessing
-  for i, (patt, flags) in enumerate(patterns):
-    try:
-      if len(flags) > 0:  # by default we use re.IGNORECASE, this gives an option to be case-sensitive.
-        # nfindall returns number of matches from findall
-        features[i] += nfindall(patt, comment, **flags)
-      else:
-        # nfindall returns number of matches from findall
-        features[i] += nfindall(patt, comment)
-    except:
-      import traceback
-      print(traceback.format_exc())
 
-  check_punct = r"([\#\$\!\?\.\:\;\(\)\"\',\[\]/]{1,}|\.\.\.)"  # check for punctuation
-  sentences = comment.split("\n")[:-1]  # as per processing, we have extra \n at end
-  if len(word_tags) > 0:  # extract just word from each word/tag pair
-    retrieve_word = r"(/?\w+)(?=/)"  # they are separated by a / with tag
-    extract_words = [findall(retrieve_word, word) for word in word_tags]
-    extract_words = [w[0].lower() for w in extract_words if
-                     len(w) > 0]  # they are lists from findall
-  if len(sentences) > 0:  # check divide by 0 errors.
-    # 15. Avg n tokens in sentence
-    # nfindall return number of matches from findall
-    features[14] = sum(
-      [nfindall(r"(\b/.{0,4}(?:\s|$)|"+f"{check_punct}/"+r"[\-]?.{0,4}[\-]?(?:\s|$))", s) for s in sentences]) / len(
-      sentences)
-    # 17. n sentences
-    features[16] = len(sentences)
+  features_array = np.zeros((1, 173))
 
-  if len(word_tags) > 0:
-    # 16. Avg len tokens excluding punctutation only, in chars
-    # findall returns all matches
-    # below returns None for tokens proceeded by only punctuation.
-    valid_tokens = [w for w in word_tags if re.match(rf"{check_punct}/", w) is None]
-    if len(extract_words) > 0:
-      features[15] = len("".join([v[:v.rfind('/')] for v in valid_tokens])) / (len(
-        valid_tokens))  # n chars / n tokens
+  body = re.compile("([\w]+|[\W]+)/(?=[\w]+|[\W]+)").findall(comment)  # left
+  lemma = re.compile("(?=[\w]+|[\W]+)/([\w]+|[\W]+)").findall(comment) #right
 
-  # Norms
-  if len(word_tags) > 0:  # extract just word from each word
-    # retrieve_word = r"(\w+)(?=/)"  # they are separated by a / with token
-    chosen_bgl = []
-    for x in extract_words:  # this is faster than pd.isin, but ugly -_-
-      try:
-        chosen_bgl.append(bgl2.loc[x])  # some words might not have a value,
-        # this gets a row as a series with the column values intact.
-      except:
-        pass
-    # Below are all features Bristol Gillhooly and Logie
-    # Start with AoA
-    AoA = [x.get("AoA (100-700)", np.nan) for x in chosen_bgl]
-    AoA = flatten(AoA)
-    # Do the two BGL, AoA cases
-    if np.count_nonzero(~np.isnan(AoA)) > 0:
-      # 18. norms average AoA
-      features[17] = np.nanmean(AoA)
-      # 21. standard deviation AoA
-      features[20] = np.nanstd(AoA)
-    # now IMG
-    IMG = [x.get("IMG", np.nan) for x in chosen_bgl]
-    IMG = flatten(IMG)
-    if np.count_nonzero(~np.isnan(IMG)) > 0:
-      # 19. average IMG
-      features[18] = np.nanmean(IMG)
-      # 22. standard deviation IMG
-      features[21] = np.nanstd(IMG)
+  # 1. Number of tokens in uppercase ( 3 letters long)
+  pattern = re.compile('[A-Z]{3,}\/\S+')
+  result = pattern.findall(comment)
+  features_array[0][0] = len(result)
 
-    # finally FAM
-    FAM = [x.get("FAM", np.nan) for x in chosen_bgl]
-    FAM = flatten(FAM)
-    if np.count_nonzero(~np.isnan(FAM)) > 0:
-      # 20. average FAM
-      features[19] = np.nanmean(FAM)
-      # 23. standard deviation FAM
-      features[22] = np.nanstd(FAM)
+  # 2. Number of first-person pronouns
+  result = re.compile(r'\b(' + r'|'.join(FIRST_PERSON_PRONOUNS) + r')\b').findall(comment)
+  features_array[0][1] = len(result)
 
-    # Now we start Warringer norms
-    chosen_warr = []
-    for x in extract_words:  # again, fastest method
-      try:
-        chosen_warr.append(warr2.loc[x])  # some words might not have a value
-      except:
-        pass
-    # first V.Mean.Sum
-    VMS = [x.get("V.Mean.Sum", np.nan) for x in chosen_warr]
-    VMS = flatten(VMS)
-    if np.count_nonzero(~np.isnan(VMS)) > 0:
-      # 24. average V.Mean.Sum
-      features[23] = np.nanmean(VMS)
-      # 27. standard deviation V.Mean.Sum
-      features[26] = np.nanstd(VMS)
-    # second A.Mean.Sum
-    AMS = [x.get("A.Mean.Sum", np.nan) for x in chosen_warr]
-    AMS = flatten(AMS)
-    if np.count_nonzero(~np.isnan(AMS)) > 0:
-      # 25. average A.Mean.Sum
-      features[24] = np.nanmean(AMS)
-      # 28. standard deviation A.Mean.Sum
-      features[27] = np.nanstd(AMS)
-    # third D.Mean.Sum
-    DMS = [x.get("D.Mean.Sum", np.nan) for x in chosen_warr]
-    DMS = flatten(DMS)
-    if np.count_nonzero(~np.isnan(DMS)) > 0:
-      # 26. average D.Mean.Sum
-      features[25] = np.nanmean(DMS)
-      # 29. standard deviation D.Mean.Sum
-      features[28] = np.nanstd(DMS)
+  # 3. Number of second-person pronouns
+  result = re.compile(r'\b(' + r'|'.join(SECOND_PERSON_PRONOUNS) + r')\b').findall(comment)
+  features_array[0][2] = len(result)
 
-  return features
+  # 4. Number of third-person pronouns
+  result = re.compile(r'\b(' + r'|'.join(THIRD_PERSON_PRONOUNS) + r')\b').findall(comment)
+  features_array[0][3] = len(result)
 
+  # 5. Number of coordinating conjunctions
+  features_array[0][4] = lemma.count('CC')
 
+  # 6. Number of past-tense verbs
+  features_array[0][5] = lemma.count('VBD')
 
+  # 7. Number of future-tense verbs
+  pattern1 = re.compile('((\'ll\/MD\w*|will\/MD\w*|gonna\/\w+)\s+\w+\/VB)')
+  pattern2 = re.compile('(go\/VB\w*\s+to\/TO\w*\s+\w+\/VB)')
+  result1 = pattern1.findall(comment)
+  result2 = pattern2.findall(comment)
+  features_array[0][6] = len(result1) + len(result2)
+
+  # 8. Number of commas
+  pattern = re.compile('\S+/,')
+  result = pattern.findall(comment)
+  features_array[0][7] = len(result)
+
+  # 9. Number of multi-character punctuation tokens
+  pattern = re.compile('([?!,;:\.\-`"]{2,})\/')
+  result = pattern.findall(comment)
+  features_array[0][8] = len(result)
+
+  # 10. Number of common nouns
+  features_array[0][9] = lemma.count('NN') + lemma.count('NNS')
+
+  # 11. Number of proper nouns
+  features_array[0][10] = lemma.count('NNP') + lemma.count('NNPS')
+
+  # 12. Number of adverbs
+  features_array[0][11] = lemma.count('RB') + lemma.count('RBR') + lemma.count('RBS')
+
+  # 13. Number of wh- words
+  features_array[0][12] = lemma.count('WDT') +  lemma.count('WP') + lemma.count('WP$') + lemma.count('WRB')
+
+  # 14. Number of slang acronyms
+  result = re.compile(r'\b(' + r'|'.join(SLANG) + r')\b').findall(comment)
+  features_array[0][13] = len(result)
+
+  # 15. Average length of sentences, in tokens
+  sentence_amount = comment.count('\n')+1
+  features_array[0][14] = len(body)/sentence_amount
+
+  # 16. Average length of tokens, excluding punctuation-only tokens, in characters
+  num_token = 0
+  sum_token = 0
+  for e in body:
+      if e[0] not in string.punctuation:
+          num_token += 1
+          sum_token += len(e)
+  if num_token!=0:
+    features_array[0][15]
+
+  # 17. Number of sentences.
+  features_array[0][16] = comment.count('\n') + 1
+
+  # prepare for 18 - 23
+  sAoA = []
+  sIMG = []
+  sFAM = []
+  valid_word_count = 0
+  for e in body:
+      if e in word1:
+          valid_word_count += 1
+          i = word1.index(e)
+          sAoA.append(int(AoA[i]))
+          sIMG.append(int(IMG[i]))
+          sFAM.append(int(FAM[i]))
+
+  # 18. Average of AoA (100-700) from Bristol, Gilhooly, and Logie norms
+  # 19. Average of IMG from Bristol, Gilhooly, and Logie norms
+  # 20. Average of FAM from Bristol, Gilhooly, and Logie norms
+  # 21. Standard deviation of AoA (100-700) from Bristol, Gilhooly, and Logie norms
+  # 22. Standard deviation of IMG from Bristol, Gilhooly, and Logie norms
+  # 23. Standard deviation of FAM from Bristol, Gilhooly, and Logie norms
+  if valid_word_count == 0:
+      features_array[0][17:22] = [0,0,0,0,0,0]
+  else:
+      features_array[0][17] = np.mean(sAoA)
+      features_array[0][20] = np.std(sAoA)
+      features_array[0][18] = np.mean(sIMG)
+      features_array[0][21] = np.std(sIMG)
+      features_array[0][19] = np.mean(sFAM)
+      features_array[0][22] = np.std(sFAM)
+
+  # prepare for 24 - 29
+  s_valence = []
+  s_dominance = []
+  s_arousal = []
+  valid_word_count = 0
+  for e in body:
+      if e in word2:
+          valid_word_count += 1
+          i = word2.index(e)
+          s_valence.append(float(valence[i]))
+          s_dominance.append(float(dominance[i]))
+          s_arousal.append(float(arousal[i]))
+
+  # 24. Average of V.Mean.Sum from Warringer norms
+  # 25. Average of A.Mean.Sum from Warringer norms
+  # 26. Average of D.Mean.Sum from Warringer norms
+  # 27. Standard deviation of V.Mean.Sum from Warringer norms
+  # 28. Standard deviation of A.Mean.Sum from Warringer norms
+  # 29. Standard deviation of D.Mean.Sum from Warringer norms
+  if valid_word_count == 0:
+      features_array[0][23:28] = [0,0,0,0,0,0]
+  else:
+      features_array[0][23] = np.mean(s_valence)
+      features_array[0][26] = np.std(s_valence)
+      features_array[0][24] = np.mean(s_dominance)
+      features_array[0][27] = np.std(s_dominance)
+      features_array[0][25] = np.mean(s_arousal)
+      features_array[0][28] = np.std(s_arousal)
+
+  return features_array
 
 
 def extract2(feat, comment_class, comment_id):
@@ -254,12 +238,26 @@ def extract2(feat, comment_class, comment_id):
         function adds feature 30-173). This should be a modified version of
         the parameter feats.
     '''
-    class_file = files[comment_class]
-    if comment_id in class_file[0]:  # classfile[0] is the dict from before.
-        # if its in, we are safe to access the value, which is the line # aka which feature to take.
-        feat[29:] = class_file[1][class_file[0][comment_id], :-1]  # we take up to the label, as per doc string.
-    else:
-        print('not found')
+    if comment_class == 'Left':
+        feat[0][-1] = 0
+        index = Left_ID.index(comment_id)
+        feat[0][29:-1] = Left_data[index][:]
+
+    if comment_class == 'Center':
+        feat[0][-1] = 1
+        index = Center_ID.index(comment_id)
+        feat[0][29:-1] = Center_data[index][:]
+
+    if comment_class == 'Right':
+        feat[0][-1] = 2
+        index = Right_ID.index(comment_id)
+        feat[0][29:-1] = Right_data[index][:]
+
+    if comment_class == 'Alt':
+        feat[0][-1] = 3
+        index = Alt_ID.index(comment_id)
+        feat[0][29:-1] = Alt_data[index][:]
+
     return feat
 
 
@@ -270,20 +268,21 @@ def main(args):
     data = json.load(open(args.input))
     feats = np.zeros((len(data), 173+1))
 
+    # load files
+
+
+
     # TODO: Call extract1 for each datatpoint to find the first 29 features.
     # Add these to feats.
     # TODO: Call extract2 for each feature vector to copy LIWC features (features 30-173)
     # into feats. (Note that these rely on each data point's class,
     # which is why we can't add them in extract1).
 
-    for i, comment in enumerate(data):
-        if (i + 1) % 500 == 0:
-            print(f"step: '{i+1}' at time '{time.clock()-stime}'")
-
-        feats[i, :-1] = extract1(comment['body'])
-        feats[i, :-1] = extract2(feats[i, :-1], comment['cat'], comment['id'])
-        class_file = files[comment['cat']]
-        feats[i, -1] = class_file[1][0, -1]  # adding the label since extract2 doesn't do that.
+    for i in range(feats.shape[0]):
+        feats[i][:-1] = extract1(data[i]['body'])
+        feats[i][:-1] = extract2(feats[i, :-1], data[i]['cat'], data[i]['id'])
+        if i % 100 == 0:
+            print(i)
 
     np.savez_compressed(args.output, feats)
 
